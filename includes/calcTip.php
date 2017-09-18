6014c142fdf814cecfae13e739b08dce68af8155
Peg Ostlund <postlund017@gmail.com>
2017-09-18 17:25:11 -0400

    Made progress with the logic processing

--- includes/calcTip.php
+++ includes/calcTip.php
@@ -2,8 +2,55 @@
 
 require('helpers.php');
 
+// initialize variables
+$newTransaction = false;
+$totalBill = 0;
+
 dump($_GET);
 
-$billAmount = $_GET["billAmount"];
+if (empty($_GET)) {
+   $pleasePay = false;
+   $billAmount = '';
+   $newTransaction = true;
+   return;
+}
 
-dump($billAmount);
+// initialize variable - I am 'using' the nature of a loosely typed language. Should I?
+$pleasePay = 0;
+
+if (isset($_GET['billAmount'])) {
+  $billAmount = $_GET['billAmount'];
+}
+
+if ($billAmount == '') {
+   return $billAmount;
+}
+
+if (isset($_GET['tip'])) {
+  $tip = $_GET['tip'];
+}
+
+if (isset($_GET['round'])) {        // if 'round' is set we should round
+  $round = true;
+}
+else {                              // if 'round' is not set, we should round
+   $round = false;
+}
+
+if (isset($_GET['divideBy'])) {     // get the amount to divide by
+  $divideBy = $_GET['divideBy'];
+}
+
+//dump($billAmount);
+//dump($tip);
+//dump($totalBill);
+
+$totalBill = ($billAmount + ($billAmount * $tip));
+dump($totalBill);
+
+$pleasePay = ($totalBill / $divideBy);
+if ($round) {
+   $pleasePay = round($pleasePay);  // round defaults to whole numbers
+}
+
+dump($pleasePay);
--- index.php
+++ index.php
@@ -1,10 +1,10 @@
-<?php require("includes/calcTip.php"); ?>
+<?php require('includes/calcTip.php'); ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang='en'>
     <head>
         <title>My Form</title>
-        <meta charset="utf-8" />
+        <meta charset='utf-8' />
 
         <link href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css' rel='stylesheet'>
         <link href='css/p2.css' rel='stylesheet'>
@@ -12,34 +12,43 @@
     </head>
     <body>
         <!-- Start Body -->
-        <h1>Tip Calculator</h1>
+        <h1>American Tip Calculator</h1>
         <form method='GET'>
-                <div class="form-group">
-                  <label for="billAmount">Bill Amount:</label>
-                  <input name="billAmount" type="text" />
+                <div class='form-group'>
+                  <label for='billAmount'>Bill Amount:</label>
+                  <input name='billAmount' id='billAmount' type='text' value='<?=$billAmount?>'>
                 </div>
 
-                <div class="form-group">
-                  <label for="tip">Tip:</label>
-                  <select name="tip">
-                      <option value="10">Poor (10%)</option>
-                      <option value="15" selected>Average (15%)</option>
-                      <option value="18">Good (18%)</option>
-                      <option value="20">Excellent (20%)</option>
+                <div class='form-group'>
+                  <label for='tip'>Tip:</label>
+                  <select name='tip' id='tip'>
+                      <option value='.10'>Poor (10%)</option>
+                      <option value='.15' selected>Average (15%)</option>
+                      <option value='.18'>Good (18%)</option>
+                      <option value='.20'>Excellent (20%)</option>
                   </select>
                 </div>
 
-                <div class="form-group"
-                  <label for="round">Round:</label>
-                  <input name="round" type="checkbox" checked>
+                <div class='form-group'>
+                  <label for='round'>Round:</label>
+                  <input name='round' id='round' type='checkbox'>
                 </div>
 
-                <div class="form-group">
-                  <label for="divideBy">Divide By:</label>
-                  <input name="divideBy" type="number" min=1 max=16 />
+                <div class='form-group'>
+                  <label for='divideBy'>Divide By:</label>
+                  <input name='divideBy' id='divideBy' type='number' min='1' max='100' step='1' value='2' required/>
                 </div>
 
-                <input type="submit" value="Calculate" />
+                <input type='submit' value='Calculate' />
+
+                <?php if (!$newTransaction) : ?>
+                   <?php if (!$pleasePay) : ?>
+                       <div class='alert alert-warning'>You did not enter a valid amount for the total the bill.</div>
+                   <?php else : ?>
+                       <div class='alert alert-info'>The total amount including tip is: <strong>$<?=sanitize($totalBill)?></strong>, or: <strong>$<?=sanitize($pleasePay)?></strong> per person.</div>
+                   <?php endif; ?>
+                <?php endif; ?>
+
         </form>
         <!-- End Body -->
     </body>
