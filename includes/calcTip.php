6a3a49fabeb796f7e2ce62646cf527f591d87e5b
Peg Ostlund <postlund017@gmail.com>
2017-09-20 11:34:58 -0400

    Works ok with basic testing.

--- includes/calcTip.php
+++ includes/calcTip.php
@@ -5,35 +5,36 @@ require('helpers.php');
 // initialize variables
 $newTransaction = false;
 $totalBill = 0;
+$pleasePay = 0;
 
-dump($_GET);
+//dump($_GET);
 
-if (empty($_GET)) {
-   $pleasePay = false;
+if (empty($_GET)) {        //If $_GET is empty it's a new page. Set some default values and return.
    $billAmount = '';
    $newTransaction = true;
+   $round = false;
+   $tip = 15;
+   $divideBy = 2;
+   //dump($tip);
    return;
 }
-
-// initialize variable - I am 'using' the nature of a loosely typed language. Should I?
-$pleasePay = 0;
 
 if (isset($_GET['billAmount'])) {
   $billAmount = $_GET['billAmount'];
 }
 
-if ($billAmount == '') {
-   return $billAmount;
-}
-
 if (isset($_GET['tip'])) {
   $tip = $_GET['tip'];
+  //dump($tip);
+  $percentage = $tip / 100;
+  //dump($tip);
+  //dump($percentage);
 }
 
 if (isset($_GET['round'])) {        // if 'round' is set we should round
   $round = true;
 }
-else {                              // if 'round' is not set, we should round
+else {                              // if 'round' is not set, we should NOT round
    $round = false;
 }
 
@@ -43,14 +44,23 @@ if (isset($_GET['divideBy'])) {     // get the amount to divide by
 
 //dump($billAmount);
 //dump($tip);
-//dump($totalBill);
+//dump($totalBill
 
-$totalBill = ($billAmount + ($billAmount * $tip));
-dump($totalBill);
+/* This is a place where an error should be shown - when we do validation. It is the
+ * last check in the code because other values are set up to use defaults or are more
+ * likely to be set because of the type of input (select list, for example) and
+ * must be valid when the error returns so that the page can retain those entries.
+*/
+if ($billAmount == '') {
+   return $billAmount;
+}
+
+$totalBill = ((float) $billAmount + ((float) $billAmount * (float) $percentage));
+//dump($totalBill);
 
 $pleasePay = ($totalBill / $divideBy);
 if ($round) {
    $pleasePay = round($pleasePay);  // round defaults to whole numbers
 }
 
-dump($pleasePay);
+//dump($divideBy);
--- index.php
+++ index.php
@@ -5,50 +5,45 @@
     <head>
         <title>My Form</title>
         <meta charset='utf-8' />
-
         <link href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css' rel='stylesheet'>
         <link href='css/p2.css' rel='stylesheet'>
-
     </head>
     <body>
-        <!-- Start Body -->
+    <!-- Start Body -->
         <h1>American Tip Calculator</h1>
         <form method='GET'>
-                <div class='form-group'>
-                  <label for='billAmount'>Bill Amount:</label>
-                  <input name='billAmount' id='billAmount' type='text' value='<?=$billAmount?>'>
-                </div>
-
-                <div class='form-group'>
-                  <label for='tip'>Tip:</label>
-                  <select name='tip' id='tip'>
-                      <option value='.10'>Poor (10%)</option>
-                      <option value='.15' selected>Average (15%)</option>
-                      <option value='.18'>Good (18%)</option>
-                      <option value='.20'>Excellent (20%)</option>
-                  </select>
-                </div>
-
-                <div class='form-group'>
-                  <label for='round'>Round:</label>
-                  <input name='round' id='round' type='checkbox'>
-                </div>
-
-                <div class='form-group'>
-                  <label for='divideBy'>Divide By:</label>
-                  <input name='divideBy' id='divideBy' type='number' min='1' max='100' step='1' value='2' required/>
-                </div>
-
-                <input type='submit' value='Calculate' />
-
-                <?php if (!$newTransaction) : ?>
-                   <?php if (!$pleasePay) : ?>
-                       <div class='alert alert-warning'>You did not enter a valid amount for the total the bill.</div>
-                   <?php else : ?>
-                       <div class='alert alert-info'>The total amount including tip is: <strong>$<?=sanitize($totalBill)?></strong>, or: <strong>$<?=sanitize($pleasePay)?></strong> per person.</div>
-                   <?php endif; ?>
-                <?php endif; ?>
-
+        <!-- Start Form -->
+          <div class='form-group'>
+             <label for='billAmount'>Bill Amount:</label>
+             <input name='billAmount' id='billAmount' type='text' value='<?=$billAmount?>'>
+             <span class="hint">(Required)</span>
+          </div>
+          <div class='form-group'>
+            <label for='tip'>Tip:</label>
+            <select name='tip' id='tip'>
+                <option <?php if ($tip == '10') echo 'SELECTED'?> value='10'>Poor (10%)</option>
+                <option <?php if ($tip == '15') echo 'SELECTED'?> value='15'>Average (15%)</option>
+                <option <?php if ($tip == '18') echo 'SELECTED'?> value='18'>Good (18%)</option>
+                <option <?php if ($tip == '20') echo 'SELECTED'?> value='20'>Excellent (20%)</option>
+            </select>
+          </div>
+          <div class='form-group'>
+             <label for='round'>Round:</label>
+             <input name='round' id='round' type='checkbox'<?php if (($round)) echo 'CHECKED'?>>
+          </div>
+          <div class='form-group'>
+             <label for='divideBy'>Divide By:</label>
+             <input name='divideBy' id='divideBy' type='number' min='1' max='100' step='1' value='<?=$divideBy?>' required/>
+          </div>
+          <input type='submit' value='Calculate' />
+          <?php if (!$newTransaction) : ?>
+             <?php if (!$pleasePay) : ?>
+                <div class='alert alert-warning'>You did not enter a valid amount for the total the bill.</div>
+             <?php else : ?>
+                <div class='alert alert-success'>The total amount including tip is: <strong>$<?=sanitize($totalBill)?></strong>, or: <strong>$<?=sanitize($pleasePay)?></strong> per person.</div>
+             <?php endif; ?>
+          <?php endif; ?>
+        <!-- End Form -->
         </form>
         <!-- End Body -->
     </body>
